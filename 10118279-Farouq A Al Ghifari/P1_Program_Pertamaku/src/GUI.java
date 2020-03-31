c83455173ccd25a0a485be1e772be1e82b657793
Farouq Addaruquthni Al Ghifari <62788416+Farouq17@users.noreply.github.com>
2020-03-31 13:49:10 +0700

    Add files via upload

---
+++ 10118279-Farouq A Al Ghifari/P1_Program_Pertamaku/src/GUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+import javax.swing.JFrame;
+
+public class GUI {
+    public static void main(String[] args) {
+        JFrame futama = new JFrame();
+        futama.setSize(300, 200);
+        futama.setTitle("Program GUI Pertama");
+        futama.setVisible(true);
+    }
+}
